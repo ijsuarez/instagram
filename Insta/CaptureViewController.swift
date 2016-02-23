9c10b44e407ed5996a4e510afb3b389ed5529aa7
Adam Epstein <Labuser@wtk316-04.cec.wustl.edu>
2016-02-23 17:24:30 -0600

    user can upload images via parse

--- Insta/CaptureViewController.swift
+++ Insta/CaptureViewController.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
-class CaptureViewController: UIViewController {
+class CaptureViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageToUpload: UIImageView!
+    @IBOutlet weak var captionText: UITextField!
+    @IBOutlet weak var uploadView: UIView!
+    
+    let tapUploadView = UITapGestureRecognizer()
+    let vc = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            vc.sourceType = UIImagePickerControllerSourceType.Camera
+        } else {
+            vc.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        }
+        
+        tapUploadView.addTarget(self, action: "getImage")
+        uploadView.addGestureRecognizer(tapUploadView)
+        uploadView.userInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +40,37 @@ class CaptureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        //let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        
+        imageToUpload.image = editedImage
+        uploadView.hidden = true
+        
+        self.dismissViewControllerAnimated(true) { () -> Void in
+            print("image dismissed")
+        }
+    }
+    
+    func getImage() {
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    func resize(image: UIImage, newSize: CGSize) -> UIImage {
+        let resizeImageView = UIImageView(frame: CGRectMake(0, 0, newSize.width, newSize.height))
+        resizeImageView.contentMode = UIViewContentMode.ScaleAspectFill
+        resizeImageView.image = image
+
+        UIGraphicsBeginImageContext(resizeImageView.frame.size)
+        resizeImageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+    
+    @IBAction func onSubmit(sender: AnyObject) {
+        UserMedia.postUserImage(imageToUpload.image, withCaption: captionText.text, withCompletion: nil)
+    }
 
     /*
     // MARK: - Navigation
